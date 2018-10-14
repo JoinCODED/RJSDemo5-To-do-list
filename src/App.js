1af6eac36c019ca1a3e6976f7791b3673ab74aae
Lailz <lailaabdulraheem@gmail.com>
2018-10-14 11:11:32 +0300

    Demo solved

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
+import axios from "axios";
 
 import "font-awesome/css/font-awesome.min.css";
 import "./App.css";
 
-// Data
-import items from "./data";
-
 //Components
 import ToDoList from "./ToDoList";
 
-function App() {
-  return (
-    <div className="rectangle">
-      <p className="title">TO DO LIST</p>
-      <ToDoList items={items} />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tasksFromAPI: []
+    };
+  }
+  componentDidMount() {
+    axios
+      .get("http://127.0.0.1:8000/api/list/")
+      .then(response => response.data)
+      .then(tasksFromAPI => this.setState({ tasksFromAPI: tasksFromAPI }));
+  }
+  render() {
+    return (
+      <div className="rectangle">
+        <p className="title">TO DO LIST</p>
+        <ToDoList items={this.state.tasksFromAPI} />
+      </div>
+    );
+  }
 }
 
 export default App;
